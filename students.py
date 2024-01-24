6dfc30fb25f7aeca924380946685d22593e28ae2
PerthTheIamCat <thongcum2546@gmail.com>
2024-01-24 18:03:06 +0700

    new feature : updata student by ID

--- students.py
+++ students.py
@@ -57,10 +57,35 @@ def add_new_student():
         }
         all_student = list(collection.find())
         if(next((s for s in all_student if s["_id"] == data["_id"]), None)):
-            return jsonify( {"error":"Cannot create new student"})
+            return jsonify( {"error":"Cannot create new student"}),500
         else:
             collection.insert_one(new_student)
-            return jsonify(new_student)
+            return jsonify(new_student),200
+        
+    except Exception as e:
+        print(e)
+    finally:
+        client.close()
+
+@app.route("/students/<string:std_id>", methods=["PUT"])
+def update_student(std_id):
+    try:
+        client = MongoClient(uri, tlsCAFile=ca)
+        db = client["students"]
+        collection = db["std_info"]
+        data = request.get_json()
+        new_fullname = {"$set":{"fullname":data["fullname"]}}
+        new_major = {"$set":{"major":data["major"]}}
+        new_gpa = {"$set":{"gpa":data["gpa"]}}
+        all_student = list(collection.find())
+        if(next((s for s in all_student if s["_id"] == data["_id"]), None)):
+            print("Found")
+            collection.update_one({"_id" : std_id}, new_fullname)
+            collection.update_one({"_id" : std_id}, new_major)
+            collection.update_one({"_id" : std_id}, new_gpa)
+            return jsonify(data),200
+        else:
+            return jsonify({"error":"Student not found"}),404
         
     except Exception as e:
         print(e)
@@ -77,4 +102,5 @@ try:
 except Exception as e:
         print(e)
 finally:
+    print("disconnected")
     client.close()
